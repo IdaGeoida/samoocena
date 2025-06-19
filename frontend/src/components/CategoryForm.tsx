1fda2b282d59217bd4b596933400aee61e911eac
IdaGeoida <93858477+IdaGeoida@users.noreply.github.com>
2025-06-19 19:47:57 +0200

    Deduplicate categories on fetch

--- frontend/src/components/CategoryForm.tsx
+++ frontend/src/components/CategoryForm.tsx
@@ -13,7 +13,12 @@ export default function CategoryForm({ onSubmit }: Props) {
   const [categories, setCategories] = React.useState<Category[]>([])
 
   useEffect(() => {
-    axios.get<Category[]>('/api/categories/').then(res => setCategories(res.data))
+    axios.get<Category[]>('/api/categories/').then(res => {
+      const unique = res.data.filter(
+        (c, idx, arr) => arr.findIndex((x) => x.id === c.id) === idx,
+      )
+      setCategories(unique)
+    })
   }, [])
 
   const submit = handleSubmit((data) => {
